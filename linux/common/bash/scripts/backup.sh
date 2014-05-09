6fdfdb42791898b252d38bff484e2480f4a769a7
Chuanlong Du <duchuanlong+bitbucket@gmail.com>
2014-05-09 15:47:24 +0000

    backup.sh created online with Bitbucket

---
+++ linux/common/bash/scripts/backup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function backup.usage(){
+    cat << EOF
+Description
+Syntax: backup src_dir des_dir
+EOF
+}
+
+function backup(){
+    if [ "$1" == "-h" ]; then
+        backup.usage
+        return 0
+    fi
+    echo "Backing up directory \"$1\" ..."
+    rsync -a "$1" "$2_$(date +%Y%m%d)"    
+    echo "Done."
+}
+
+if [ "$0" == ${BASH_SOURCE[0]} ]; then
+    backup $@
+fi
