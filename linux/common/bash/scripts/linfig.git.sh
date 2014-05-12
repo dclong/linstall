bf2c8b9df29e80e18cfd2bb1db5b56deb795f41d
Chuanlong Du <duchuanlong+bitbucket@gmail.com>
2014-05-12 19:19:51 +0000

    linfig.git.sh edited online with Bitbucket

--- linux/common/bash/scripts/linfig.git.sh
+++ linux/common/bash/scripts/linfig.git.sh
@@ -11,6 +11,13 @@ EOF
 function linfig.git(){
     echo "Configuring Git ..."
     # configuration
+    if [[ "$(uname -a)" == CYGWIN_NT* ]]; then
+        ln -sf "$lommon/git/gitconfig" "$HOME/.gitconfig"
+        echo "\"$HOME/.gitconfig\" -> \"$lommon/git/gitconfig\""
+        ln -sf "$lommon/git/gitignore" "$HOME/.gitignore"
+        echo "\"$HOME/.gitignore\" -> \"$lommon/git/gitignore\""
+        return 0
+    fi
     ln -Tsvf "$lommon/git/gitconfig" "$HOME/.gitconfig"
     ln -Tsvf "$lommon/git/gitignore" "$HOME/.gitignore"
     echo "Done."
