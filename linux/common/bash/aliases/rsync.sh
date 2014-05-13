49a4da178a904099037ed0690037be1db817897c
Chuanlong Du <duchuanlong+bitbucket@gmail.com>
2014-05-13 19:39:46 +0000

    rsync.sh edited online with Bitbucket

--- linux/common/bash/aliases/rsync.sh
+++ linux/common/bash/aliases/rsync.sh
@@ -21,6 +21,7 @@ alias rsync4nb205='rsync4server $(whoami)@$nb205 $ssh_port'
 alias rsync4home='rsync4server $(whoami)@$homeip $ssh_port'
 alias rsync4ubsas='rsync4server $(whoami)@${ubsas} 22'
 
-alias backup.thunderbird='backup $HOME/.thunderbird $HOME/btsync/backup/tbp'
+alias backup.thunderbird='backup $HOME/.thunderbird "$backup_dir/tbp"'
 alias backup.tbp='backup.thunderbird'
-
+alias backup.documents='backup "$documents" "$backup_dir/documents"'
+alias backup.doc='backup.documents'
