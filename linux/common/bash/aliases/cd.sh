a5f655133250c15c20731acd1dafdaae13801dc0
Ben Chuanlong Du <duchuanlong+git@gmail.com>
2014-08-15 21:48:33 -0400

    updated cd related aliases; removed useless scripts

--- linux/common/bash/aliases/cd.sh
+++ linux/common/bash/aliases/cd.sh
@@ -3,13 +3,13 @@ alias cs.1='cs ..'
 alias cd.2='cd ../..'
 alias cs.2='cs ../..'
 alias cd.3='cd ../../..'
-alias cd.documents="cd $documents"
-alias cd.doc="cd.documents"
-
 alias cs.3='cs ../../..'
 alias cd.4='cd ../../../..'
 alias cs.4='cs ../../../..'
 alias cd.5='cd ../../../../..'
 alias cs.5='cs ../../../../..'
-alias cs.documents="cs $documents"
-alias cs.doc="cs.documents"
+alias cd.documents='cd $documents'
+alias cs.documents='cs $documents'
+alias cd.doc='cd.documents'
+alias cs.doc='cs.documents'
+
