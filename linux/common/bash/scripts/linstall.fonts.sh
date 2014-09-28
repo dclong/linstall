6e30cb4ab58c7f11f5bf768510c9053736afaafb
Benjamin Du <duchuanlong+git@gmail.com>
2014-09-28 00:05:40 -0400

    updated linstall.fonts.sh

--- linux/common/bash/scripts/linstall.fonts.sh
+++ linux/common/bash/scripts/linstall.fonts.sh
@@ -1,13 +1,16 @@
+#!/usr/bin/env bash
+
 function linstall.fonts.usage(){
     cat << EOF
 Install some fonts (mostly chinese related) including: ttf-arphic-uming, ttf-wqy-microhei, ttf-wqy-zenhei, xfonts-wqy and fonts-sil-abyssinica.
 Syntax: linstall.fonts
 EOF
 }
-# chinese fonts
+
 function linstall.fonts(){
     wajig install -y ttf-arphic-uming ttf-wqy-microhei ttf-wqy-zenhei xfonts-wqy fonts-sil-abyssinica
 }
+
 if [ "$0" == ${BASH_SOURCE[0]} ]; then
     linstall.fonts $@
 fi
