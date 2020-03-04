c3ee91b0bacab476492b21c7c1d8945aaf9fbe2e
Benjamin Du <longendu@yahoo.com>
2020-03-03 19:41:12 -0800

    fix test

--- test/test_web.py
+++ test/test_web.py
@@ -14,6 +14,6 @@ def test_blogging():
 def test_proxychains():
     """Test installing the blogging tools.
     """
-    cmd = "xinstall --sudo pc -ic"
+    cmd = "xinstall --sudo -y pc -ic"
     sp.run(cmd, shell=True, check=True)
 
