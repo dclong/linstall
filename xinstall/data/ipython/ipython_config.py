7b7e757496a590441e03f12cf5c12bcfa4134f02
Benjamin Du <longendu@yahoo.com>
2020-03-30 12:57:30 -0700

    more aliaes

--- xinstall/data/ipython/ipython_config.py
+++ xinstall/data/ipython/ipython_config.py
@@ -240,11 +240,25 @@ c.AliasManager.user_aliases = [
         "sudo mount -t vboxsf -o uid=$(whoami),gid=$(whoami),fmask=177,dmask=077 gdrive ${HOME}/gdrive"
     ),
     ("ls.media", "ls *.jpg *.jpeg *.png *.mp3 *.avi *.mkv *.mov *.mp4 *.wmv"),
+    ("ls.excel", "ls *.xls *.xlsx"),
+    ("ls.word", "ls *.doc *.docx"),
+    ("ls.spreadsheet", "ls *.xls *.xlsx *.csv"),
+    ("ls.data", "ls *.xls *.xlsx *.csv *.tsv"),
+    ("ls.archive", "ls *.zip *.tar.gz *.tar.xz *.tar"),
+    ("ls.zip", "ls.archive"),
+    ("ls.package", "ls *.air *.deb *.jar *.apk"),
+    ("ls.pkg", "ls *.air *.deb *.jar *.apk"),
+    ("ls.tex.aux", "ls *.aux *.bbl *.blg *.log *.toc *.synctex.gz"),
 ]
-if 'darwin' in PLATFORM:
+if "darwin" in PLATFORM:
     c.AliasManager.user_aliases.extend([
-        ('md5sum', 'md5 -r'),
+        ("md5sum", "md5 -r"),
+        ("ffmpeg.record_screen", "ffmpeg -f avfoundation -i '1' -pix_fmt yuv420p -r 25 $(date +%m%d%H%M%S).mp4"),
+        ("record_screen", "ffmpeg -f avfoundation -i '1' -pix_fmt yuv420p -r 25 $(date +%m%d%H%M%S).mp4"),
     ])
 else:
-    c.AliasManager.user_aliases.extend([])
+    c.AliasManager.user_aliases.extend([
+        ("ffmpeg.record_screen", "ffmpeg -f x11grab -r 25 -s cif -i :0.0 $(date +%m%d%H%M%S).mp4"),
+        ("record_screen", "ffmpeg -f x11grab -r 25 -s cif -i :0.0 $(date +%m%d%H%M%S).mp4"),
+    ])
 c.IPCompleter.use_jedi = False
