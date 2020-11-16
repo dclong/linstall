3c39aa3cbb19b77da2f54d1cc6b96ea1e1fc8db0
Benjamin Du <longendu@yahoo.com>
2020-11-15 16:36:49 -0800

    fix issue in brew_install_safe

--- xinstall/utils.py
+++ xinstall/utils.py
@@ -98,9 +98,7 @@ def brew_install_safe(pkgs: Union[str, list]) -> None:
         pkgs = [pkgs]
     for pkg in pkgs:
         run_cmd(
-            f"""brew install {pkg} \
-            && brew unlink {pkg} \
-            && brew link --overwrite --force {pkg}"""
+            f"brew install {pkg} && brew link --overwrite --force {pkg}"
         )
 
 
