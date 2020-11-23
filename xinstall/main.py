c76d9c18314ab9b115c9a312e9d7ae098a8aa8f3
Benjamin Du <longendu@yahoo.com>
2020-11-22 21:18:27 -0800

    bump up version

--- xinstall/main.py
+++ xinstall/main.py
@@ -102,7 +102,7 @@ logging.basicConfig(
     "%(asctime)s | %(module)s.%(funcName)s: %(lineno)s | %(levelname)s: %(message)s",
     level=logging.INFO
 )
-__version__ = "0.26.1"
+__version__ = "0.27.0"
 
 
 def version(args):
