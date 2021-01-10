b29a2d95f0b967e92ecc02fc26d5da49e9cfb560
Ben Chuanlong Du <legendu.net@outlook.com>
2021-01-10 15:23:44 -0800

    debugging info

--- xinstall/main.py
+++ xinstall/main.py
@@ -260,6 +260,7 @@ def main():
     """
     args = parse_args()
     logging.basicConfig(level=getattr(logging, args.level.upper()))
+    logging.debug("Command-line options:\n%s", json.dumps(vars(args), indent=True))
     args.func(args)
 
 
